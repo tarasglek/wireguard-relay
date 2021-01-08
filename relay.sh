e171b3309291d6077361cc3a5561ad522a055324
Taras Glek <taras@glek.net>
2021-01-07 22:26:22 -0800

    fixes

--- relay.sh
+++ relay.sh
@@ -2,10 +2,10 @@
 BASEPORT=${1:-9000}
 PAIRS=${2:-10}
 trap "pkill -P $$ --signal 9" EXIT
-for (( COUNTER=0; COUNTER<=PAIRS; COUNTER+=1 )); do
+for (( COUNTER=0; COUNTER<PAIRS; COUNTER+=1 )); do
     EVEN=$((BASEPORT + COUNTER * 2))
     ODD=$((EVEN+1))
     echo Forwarding between port $EVEN-$ODD
-    socat udp-listen:$EVEN,reuseaddr udp-listen:$ODD,reuseaddr &
+    socat udp-listen:$EVEN,reuseaddr,fork udp-listen:$ODD,reuseaddr,fork &
 done
 wait
